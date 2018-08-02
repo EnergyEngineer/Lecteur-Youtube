204cfef78f113424da0c353d77ea29601855f932
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-02 15:54:16 +0200

    ChansonCell

---
+++ Lecteur Youtube/ChansonCell.swift
@@ -0,0 +1,39 @@
+//
+//  ChansonCell.swift
+//  Lecteur Youtube
+//
+//  Created by TOGNI Elie on 02/08/2018.
+//  Copyright © 2018 TOGNI Elie. All rights reserved.
+//
+
+import UIKit
+
+class ChansonCell: UITableViewCell {
+
+    @IBOutlet weak var miniature: UIImageView!
+    @IBOutlet weak var artisteEtTitreLabel: UILabel!
+    
+    var chanson: Chanson! //Forcée en non optionnelle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+   
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    func creerCell(_ chanson: Chanson){
+        self.chanson = chanson
+        
+        let attributed = NSMutableAttributedString(string: self.chanson.titre, attributes: [.font: UIFont.boldSystemFont(ofSize: 20), .foregroundColor: UIColor.black])
+        
+        let secondeLigne = NSAttributedString(string: "\n" + self.chanson.artiste, attributes: [.font: UIFont.italicSystemFont(ofSize: 20), .foregroundColor: UIColor.darkGray])
+        
+        attributed.append(secondeLigne)
+        artisteEtTitreLabel.attributedText = attributed
+    }
+
+}
--- Lecteur Youtube/TableauControleur.swift
+++ Lecteur Youtube/TableauControleur.swift
@@ -14,6 +14,8 @@ class TableauControleur: UIViewController, UITableViewDelegate, UITableViewDataS
     
     var chansons = [Chanson]() //Matrice vide
     
+    let identifiantCell = "ChansonCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -26,7 +28,22 @@ class TableauControleur: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chanson = chansons[indexPath.row] //On récupère la chanson
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: identifiantCell) as? ChansonCell {
+            cell.creerCell(chanson)
+            return cell
+        }
+        
+        
+        
         return UITableViewCell()
+       
+    }
+    
+    // Définition de la taille de cellules
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
     }
 
     func ajouterChanson(){
